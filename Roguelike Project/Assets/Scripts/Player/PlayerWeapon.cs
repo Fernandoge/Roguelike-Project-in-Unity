bb70a25cd3b45a1273fe9ce127e355d25ab2f69a
Fernandoge <fernando.gp101@gmail.com>
2019-03-30 14:20:47 -0300

    Reimported to last unity version and new gun assets added

---
+++ Roguelike Project/Assets/Scripts/Player/PlayerWeapon.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon : MonoBehaviour {
+    public GameObject bulletSpawn,bullet,curWeapon;
+    //bool gun = false;
+    public float timer;
+    float timerReset;
+    //readonly float weaponChange = 0.5f;
+    //bool changingWeapon = false;
+
+	// Use this for initialization
+	void Start () {
+        timerReset = timer;
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (timer > 0)
+            timer -= Time.deltaTime;
+
+        if (Input.GetMouseButton(0) && timer <= 0 && curWeapon != null)
+            Shoot();
+
+	}
+
+    public void SetWeapon(GameObject cur, string name, float fireRate, GameObject TypeOfBullet)
+    {
+        //changingWeapon = true;
+        curWeapon = cur;
+        //this.gun = gun;
+        timerReset = fireRate;
+        timer = timerReset;
+        bullet = TypeOfBullet;
+
+    }
+
+    public GameObject GetCurrentWeapon()
+    {
+        return curWeapon;
+    }
+
+    public void DropWeapon()
+    {
+        curWeapon.transform.position = this.transform.position;
+        curWeapon.SetActive(true);
+        SetWeapon(null,"", 0.5f, null);
+    }
+
+
+    public void Shoot()
+    {
+        Bullet bl = bullet.GetComponent<Bullet>();
+        Vector3 dir;
+        dir.x = Vector2.right.x;
+        dir.y = Vector2.right.y;
+        dir.z = 0;
+        bl.SetVals(dir, "Player");
+        Instantiate(bullet, bulletSpawn.transform.position, this.transform.rotation);
+        timer = timerReset;
+        
+    }
+}
---
+++ Roguelike Project/Assets/Scripts/Weapons/WeaponPickup.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickup : MonoBehaviour {
+    public new string name;
+    public float fireRate;
+    PlayerWeapon clsPlayerWeapon;
+    public bool onPositionToPickUp;
+    //public bool gun;
+    public GameObject weaponBullet;
+
+
+
+    // Use this for initialization
+    void Start () {
+        clsPlayerWeapon = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerWeapon>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (onPositionToPickUp && Input.GetKeyDown(KeyCode.G))
+        {
+            if (clsPlayerWeapon.GetCurrentWeapon() != null)
+            {
+                clsPlayerWeapon.DropWeapon();
+
+            }
+            //attack.SetWeapon(this.gameObject, name, fireRate, gun, weaponBullet);
+            clsPlayerWeapon.SetWeapon(this.gameObject, name, fireRate, weaponBullet);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            onPositionToPickUp = true;
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            onPositionToPickUp = false;
+    }
+}
