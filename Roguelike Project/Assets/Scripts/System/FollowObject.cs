426ab76836874c08719009907c20d38fa1d51bc0
Fernandoge <fernando.gp101@gmail.com>
2019-04-03 01:41:50 -0300

    Changed script name

---
+++ Roguelike Project/Assets/Scripts/System/FollowObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject objectToFollow;
+    bool followingObject = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (followingObject == true)
+        {
+            FollowingObjectPosition();
+        }
+
+    }
+
+    void FollowingObjectPosition()
+    {
+        Vector3 newPos = new Vector3(objectToFollow.transform.position.x, objectToFollow.transform.position.y, this.transform.position.z);
+        transform.position = newPos;
+    }
+}
