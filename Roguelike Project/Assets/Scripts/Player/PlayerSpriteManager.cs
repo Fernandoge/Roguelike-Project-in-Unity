52b58b32e6ae890f3475dd1406992900f0f37c51
Fernandoge <fernando.gp101@gmail.com>
2019-04-09 00:10:35 -0400

    Player movement animations

--- Roguelike Project/Assets/Scripts/Player/PlayerSpriteManager.cs
+++ Roguelike Project/Assets/Scripts/Player/PlayerSpriteManager.cs
@@ -6,12 +6,10 @@ public class PlayerSpriteManager : MonoBehaviour
 {
 
     public Sprite spLeft, spRight, spUp, spDown;
-    public SpriteRenderer SprRender;
-    // Use this for initialization
-    void Start()
-    {
-
-    }
+    public SpriteRenderer playerSprRender;
+    public Animator animator;
+    public PlayerMovement clsPlayerMovement;
+    public SpriteRenderer playerWeaponSprRender;
 
     // Update is called once per frame
     void Update()
@@ -19,21 +17,30 @@ public class PlayerSpriteManager : MonoBehaviour
         Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         int index = (int)((Mathf.Round(angle / 90f) + 4) % 4); //add a modulo over 4 to get a normalized index
-                                                          
+
+        if (clsPlayerMovement.moving)
+            animator.enabled = true;
+        else
+            animator.enabled = false;
+
         switch (index)
         {
             case 0:
-                SprRender.sprite = spRight;
+                playerSprRender.sprite = spRight;
+                animator.SetInteger("Direction", 0);
                 break;
             case 1:
-                SprRender.sprite = spUp;
+                playerSprRender.sprite = spUp;
+                animator.SetInteger("Direction", 1);
                 break;
             case 2:
-                SprRender.sprite = spLeft;
+                playerSprRender.sprite = spLeft;
+                animator.SetInteger("Direction", 2);
                 break;
             case 3:
-                SprRender.sprite = spDown;
+                playerSprRender.sprite = spDown;
+                animator.SetInteger("Direction", 3);
                 break;
         }
-    }
+        
 }
